7a1d81b37229674c707bd9fcc2cbe3623de48c01
ba <munkhbayrduuren@gmail.com>
2020-07-09 01:02:24 -0700

    Value utga deer ni UI delgets deer ajildag function nemsen

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 // Дэлгэцтэй ажиллах контроллер
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 // Санхүүтэй ажиллах контроллер
 var faninceController = (function () {})();
 
 // Программыг холбогч контроллер
 var appController = (function (uiController, faninceController) {
+  var DOM = uiController.getDOMstrings();
+
   var ctrlAddItem = function () {
     // 1. Оруулах өгөгдлийг дэлгэцнээс олж авна
+    console.log(uiController.getInput());
 
-    console.log("Ажиллаа");
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
     // 3. Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана.
     // 4. Төсөвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл тооцоог, дэлгэцэнд гаргана.
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
     ctrlAddItem();
   });
 
